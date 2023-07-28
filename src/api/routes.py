96f8b586e825998018e102ce49b9c39b2ad11718
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-07-28 15:52:30 +0000

    query to hello endpoint without token returns right mesaage

--- src/api/routes.py
+++ src/api/routes.py
@@ -22,3 +22,12 @@ def create_token():
 
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
+
+@api.route("/hello", methods=["GET"])
+@jwt_required()
+def get_hello():
+    dictionary = {
+        "message" : "hello world"
+    }
+    
+    return jsonify(dictionary)
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -62,17 +62,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
+			getMessage: () => {
+				const store = getStore();
+				const opts = {
+					headers: {
+						Authorization: "Bearer " + store.token
+					}
+				};
+				// fetching data from the backend
+				fetch(process.env.BACKEND_URL + "/api/hello", opts)
+					.then(resp => resp.json())
+					.then(data => setStore({ message: data.message }))
+					.catch(error => console.log("Error loading message from backend", error));
 			},
 			changeColor: (index, color) => {
 				//get the store
